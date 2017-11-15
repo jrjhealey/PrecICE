608a4a765be460be4312662b32ac6e60e7cc5990
jrjhealey <jrj.healey@gmail.com>
2017-11-15 17:14:52 +0000

    Commit with initial crystal detection and annotation code

--- PrecICE.py
+++ PrecICE.py
@@ -69,13 +69,15 @@ bounding box arithmetic to calculate sizes.
 03-09-2017 | Version 1.0 | Joe Healey <j.r.j.healey@warwick.ac.uk>
     * Initial commit with basic functionality
 
-#TODO:
+"""
+"""
+TODO
  - Report length sizes, rather than pixel sizes OR include a calculator?
   * Require user to specify pixel dimensions
   * Figure it out from a scale bar?
-
+  
+ - Switch Gaussian to bilateral filtering.
 """
-
 
 
 
@@ -102,51 +104,25 @@ __license__ = "GPLv3"
 
 # Module imports (many are non-standard library and will need to be installed).
 
-import imutils
 
-from imutils import contours
-from imutils import perspective
-from scipy.spatial import distance as dist
-import scipy.misc as sm
-import matplotlib as mpl
-mpl.use("TkAgg")
-from matplotlib import pyplot as plt
+# These imports are needed if trying to render a panel of images via Matplotlib
+#import imutils
+#from imutils import contours
+#from imutils import perspective
+#from scipy.spatial import distance as dist
+#import scipy.misc as sm
+#import matplotlib as mpl
+#mpl.use("TkAgg")
+#from matplotlib import pyplot as plt
+
+# These are required imports for basic code functionality
 import numpy as np
 import cv2
-import argparse
-import traceback
-import sys
-import logging
+import argparse, traceback, os, sys, logging
 custom_log = logging.getLogger().setLevel(logging.INFO)
 
 
-
-def midpoint(ptA, ptB):
-    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
-
-def skeletonise(image):
-    """Skeletonise an image using OpenCV operations"""
-
-    size = np.size(image)
-    skel = np.zeros(image.shape, np.uint8)
-
-    ret, image = cv2.threshold(image, 127, 255, 0)
-    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    done = False
-
-    while (not done):
-        skerode = cv2.erode(image, element)
-        temp = cv2.dilate(skerode, element)
-        temp = cv2.subtract(image, temp)
-        skel = cv2.bitwise_or(skel, temp)
-        image = skerode.copy()
-
-        zeros = size - np.count_nonzero(image)
-        if zeros == size:
-            done = True
-            return skel
-
-
+# Collect commandline arguments
 try:
     parser = argparse.ArgumentParser(
             description='Computer vision program for feature size determination. '
@@ -158,28 +134,39 @@ try:
                         action='store',
                         metavar='IMAGEFILE',
                         help='The image file to be analysed.')
-    parser.add_argument('--gkernel',
+    parser.add_argument('-o',
+                        '--out',
+                        action='store',
+                        default=None,
+                        help='Output filename stem for image saving.')
+    parser.add_argument('--kernel_size',
                         action='store',
                         type=int,
-                        default=5,
+                        default=7,
                         metavar='N',
-                        help='Kernel size for Gaussian Blurring. [Def 5]')
+                        help='Kernel size passed to relevant functions (one kernel for all functions for now). [Def 7]')
     parser.add_argument('--gsigma',
                         action='store',
                         type=int,
-                        default=0,
+                        default=2,
                         metavar='N',
                         help='Standard deviation of Gaussian Blur kernel. [Def 0]')
+    parser.add_argument('--thresh_bounds',
+                        action='store',
+                        nargs=2,
+                        metavar=('lower', 'upper'),
+                        default=[0,15],
+                        help='Lower and upper bound of pixel intensity for thresholding. [Def 0,15]')
     parser.add_argument('--ignore_intermediates',
                         action='store_true',
-                        help='Output intermediate images from processing. [Def off]')
-    parser.add_argument('--skip_denoise',
-                        action='store_true',
-                        help='Skip the Gaussian blur de-noising step.'
-                             'If the image has well defined edges it may not be necessary '
-                             'or even offer and improvement over the with-blur processing.')
+                        help='Don\'t output intermediate images from processing. [Def off]')
+    parser.add_argument('--scaling',
+                        action='store',
+                        default=0.75,
+                        help='OpenCV displays images fullsize, this scales them down to this fraction of screen size.')
 
 except NameError:
+    # This is a general error handle. Not very useful to debug, but catches malformed arguments etc
     print "An exception occured with argument parsing. Check your provided options."
     traceback.print_exc()
     sys.exit(1)
@@ -187,187 +174,216 @@ except NameError:
 
 args = parser.parse_args()
 
-print('"PrecICE" Image analysis tool, version ' + __version__ + " by " + __author__ + ' <' + __author_email__ + '>\n')
+# Welcome to the program start...
+print('"PrecICE" Image analysis tool, version %s by %s <%s>' %(__version__, __author__, __author_email__))
 
-print('Analysing image: %s \n' %args.image)
+
+### SET UP ###
 
 # List of intermediate images to store for later
+# (at present, the code collects all the intermediate images it creates
+# to assist with debugging and assessing the quality of the analysis. This
+# does mean the program runs slower and uses more memory than necessary for
+# actual analysis.
+
+
 intermediate_images = []
 intermediate_imagenames = []
 
-# IMAGE PREPROCESSING:
-logging.info(' (Step 1/) Loading image: %s' %args.image)
-# 1. Read image in to numerical array
+# Read image in to numerical array
 image = cv2.imread(args.image)
-dims = np.size(image)
+if os.path.isfile(args.image) is False:
+    print("ERROR: Could not find the file, check the filepath. Aborted.")
+    sys.exit(1)
 
+print('Analysing image: %s \n' % args.image)
+
+logging.info(' (Step 1/) Loaded image: %s' %args.image)
+
+# Get the file extension, and produce images in the same format
+ext = os.path.splitext(args.image)[1]
+
+# Store the unaltered input image
 intermediate_images.append(image)
 intermediate_imagenames.append('Original Image')
-logging.info(' (Step 2/) Original image stored...')
+logging.info('Original image stored...')
 
 
-# IMAGE PREPROCESSING:
-print('\nPreprocessing beginning...\n')
-logging.info(' (Step 3/) Image converted to 8-bit greyscale...')
-greyed = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-intermediate_images.append(greyed)
-intermediate_imagenames.append('Greyed Image')
-logging.info(' (Step 4/) Greyed image stored...')
+### PREPROCESSING IMAGE ####
+
+# Gaussian filtering seems to help slightly denoise the edges and improve the morphological
+# filtering steps to come
+
+logging.info(' (Step 2/) Gaussian Filtering image...')
+
+# Kernel sizes and sigma values may be able to be played with for more optimal results.
+blur = cv2.GaussianBlur(image, (args.kernel_size, args.kernel_size), args.gsigma)
+h, w = image.shape[:2] # Get image size
+
+# Store the blurred/Gaussian filtered image.
+intermediate_images.append(blur)
+intermediate_imagenames.append('Gaussian Filtered Image...')
+logging.info('Gaussian Filtered image stored...')
 
 
-if args.skip_denoise is False:
-    logging.info(' (Step 5/) Gaussian Filtering the image with a %d x %d kernel (standard deviation %d)' %(args.gkernel, args.gkernel, args.gsigma))
-    # Gaussian blur de-noises the image. Parameters are kernel size (5,5) and st. dev (0)
-    blurred = cv2.GaussianBlur(greyed, (args.gkernel, args.gkernel), args.gsigma)
-    intermediate_images.append(blurred)
-    intermediate_imagenames.append('Gaussian Filtered Image')
-    logging.info(' (Step 6/) Gaussian filtered image stored...')
-else:
-    logging.info(' (Step 6/) --- SKIPPING STEP 6 DE-NOISING ---')
+# Apply morphological filtering to try and close more boundaries.
+logging.info(' (Step 3/) Applying morphological gradient...')
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (args.kernel_size, args.kernel_size))
+gradient = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel)
 
 
-#### NOT SURE IF TO BINARISE OR EDGE DETECT???
+### BINARISING GRADIENT ###
 
-logging.info(' (Step 7/) Binary thresholding the image (lower 127, upper 255)...')
-retval, binary = cv2.threshold(blurred,30 ,200, cv2.THRESH_BINARY)
+logging.info(' (Step 4/) Binarising the image...')
+#lowerb = np.array([args.thresh_bounds[0] for i in xrange(2)])
+#upperb = np.array([args.thresh_bounds[1] for i in xrange(2)])
+
+# Bounds are currently arbitrarily chosen and hard-coded. This is another
+# area where some optimisation may be possible. Ideally the bounds could be
+# heuristically chosen from the input image (maybe some percentile of average
+# gray intensity or similar).
+lowerb = np.array([0, 0, 0])
+upperb = np.array([15, 15, 15])
+# Binarise the image
+binary = cv2.inRange(gradient, lowerb, upperb)
+binary_store = cv2.inRange(gradient, lowerb, upperb)
+# Store the Binarised image.
+intermediate_images.append(binary_store)
+intermediate_imagenames.append('Binarised Image')
+logging.info('Binarised image stored...')
+
+### CORRECTING EDGES ###
+
+# Now we need to exclude blobs on the edge of the image from subsequent
+# analysis by watershedding, so loop around the perimeter of the image, and if
+# the pixel is white (==255), it's a blob and we can't know it's actual dimensions
+# as it will be extending outside the of image frame, so it's floodfilled black to make it background.
+
+logging.info(' (Step 5/) Floodfilling perimeter targets...')
+for row in xrange(h):
+    if binary[row, 0] == 255:
+        cv2.floodFill(binary, None, (0, row), 0)
+    if binary[row, w-1] == 255:
+        cv2.floodFill(binary, None, (w-1, row), 0)
+
+for col in xrange(w):
+    if binary[0, col] == 255:
+        cv2.floodFill(binary, None, (col, 0), 0)
+    if binary[h-1, col] == 255:
+        cv2.floodFill(binary, None, (col, h-1), 0)
+
 intermediate_images.append(binary)
-intermediate_imagenames.append('Binarized Image')
-logging.info(' (Step 7/) Binary threshold image stored...')
+intermediate_imagenames.append('Floodfilled Image')
+logging.info('Floodfilled image stored...')
 
 
-# Canny edge detector
-logging.info(' (Step 8/) Detecting edges with Canny edge detector...')
-edged = cv2.Canny(blurred, 30, 200)
-intermediate_images.append(edged)
-intermediate_imagenames.append('Edged Image')
-logging.info(' (Step 9/) Edged image stored...')
+### FOREGROUNDING AND CREATING UNKNOWN MASK ###
+logging.info(' (Step 6/) Running cleanup mask (identifying back/foreground and unknown space)...')
+foreground = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
+foreground = cv2.morphologyEx(foreground, cv2.MORPH_CLOSE, kernel)
+intermediate_images.append(foreground)
+intermediate_imagenames.append('Foreground Image Image')
+logging.info('Foreground regions image stored...')
 
-logging.info(' (Step 10/) Beginning ...')
-dilated = cv2.dilate(edged, None, iterations=1)
-intermediate_images.append(dilated)
-intermediate_imagenames.append('Dilated Image')
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (17, 17))
 
-eroded = cv2.erode(dilated, None, iterations=1)
-intermediate_images.append(eroded)
-intermediate_imagenames.append('Eroded Image')
+background = cv2.dilate(foreground, kernel, iterations=3)
+intermediate_images.append(background)
+intermediate_imagenames.append('Background Regions Image')
+logging.info('Background regions image stored...')
+
+unknown = cv2.subtract(background, foreground)
+intermediate_images.append(foreground)
+intermediate_imagenames.append('Unknown Regions Image')
+logging.info('Unknown regions image stored...')
+
+### WATERSHEDDING IMAGE ###
+logging.info(' (Step 7/) Watershedding the image...')
+markers = cv2.connectedComponents(foreground)[1]
+# Set background intensity to 1
+markers += 1
+# Set unknown region intensity to 0
+markers[unknown == 255] = 0
+markers = cv2.watershed(image, markers)
+
+# Something to try later
+# Maybe try watershedding a blurred image to smooth the blob boundaries
+# median = cv2.medianBlur(img, 5)
+# markers2 = cv2.watershed(blur, markers)
+
+### VISUALISE WATERSHED ###
+hue_markers = np.uint8(179*np.float32(markers)/np.max(markers))
+blank_channel = 255*np.ones((h, w), dtype=np.uint8)
+marker_image = cv2.merge([hue_markers, blank_channel, blank_channel])
+marker_image = cv2.cvtColor(marker_image, cv2.COLOR_HSV2BGR)
+
+intermediate_images.append(marker_image)
+intermediate_imagenames.append('Marker Image')
+logging.info('Marker image stored...')
+
+### Create overlaid image ###
+labelled_image = image.copy()
+labelled_image[markers>1] = marker_image[markers>1]  # 1 is background color
+labelled_image = cv2.addWeighted(image, 0.5, labelled_image, 0.5, 0)
+
+intermediate_images.append(labelled_image)
+intermediate_imagenames.append('Annotated Image')
+logging.info('Annotated image stored...')
+
+## Display/Save Images
+
+if args.ignore_intermediates is False:
+    import Tkinter
+    from math import ceil
+    root = Tkinter.Tk()  # Need screen dimensions to deal with large images else they don't fit on screen.
+    s_width = root.winfo_screenwidth()
+    s_height = root.winfo_screenheight()
+    # User can specify how much they want to downscale the image themself.
+    scaling = args.scaling
+    print("Screen resolution: %s x %s" %(s_width,s_height))
+
+    for name, imageobj in zip(intermediate_imagenames, intermediate_images): # iterate list of names and images
+        print("Rendering: " + name)
+        cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+        # Resize images that are too large to display
+        cv2.resizeWindow(name, int(ceil(s_width*scaling)), int(ceil(s_height*scaling)))
+        cv2.imshow(name, imageobj)
+
+        # Write output files of intermediate images (default names used but with _ subbed in for whitespace
+        if args.out is None:
+            name = name.replace(" ", "_") + ext
+        else:
+            name = "%s%s%s%s" %(args.out, "_", name.replace(" ", "_"), ext)
+
+        cv2.imwrite(name, imageobj)
+
+    cv2.waitKey(0)
 
 
-
-# Next steps:
-# - complete the contour of the blobs (if erode-dilate isn't sufficient)
-# - fill the contour areas
-# - find pixel values > 100 etc (just blobs)
-# -
-
-
-# Output
-
-# ## Images
-# if args.ignore_intermediates is False:
-#     for name, imageobj in zip(intermediate_imagenames, intermediate_images):
-#         cv2.imshow(name, imageobj)
-#
-#     cv2.waitKey(0)
+# This is commented out until I can be bothered to work out an easy way to switch BGR to RGB for the relevant images.
+# The images will display in a panel based window, rather than in separate ones but the colours will be off.
 
 # Panel images:
 
-# OpenCV's BGR encoding needs to be converted to RGB for matplotlib to display correctly:
-mpl_converted_intermediates = [sm.toimage(image) for image in intermediate_images]
-import math
-for i in xrange(len(zip(intermediate_imagenames, mpl_converted_intermediates))):
-    rows = math.ceil(float(len(mpl_converted_intermediates))/2)
-    plt.subplot(rows,2,i+1)
-    plt.imshow(mpl_converted_intermediates[i])
-    plt.title(intermediate_imagenames[i])
-    plt.xticks([]),plt.yticks([])
-plt.show()
+#OpenCV's BGR encoding needs to be converted to RGB for matplotlib to display correctly:
+#mpl_converted_intermediates = [sm.toimage(image) for image in intermediate_images]
+# from math import ceil
+# import matplotlib as mpl
+# mpl.use("TkAgg")
+# from matplotlib import pyplot as plt
+# for i in xrange(len(zip(intermediate_imagenames, intermediate_images))):
+#     rows = ceil(float(len(intermediate_images))/2)
+#     plt.subplot(rows,2,i+1)
+#     plt.imshow(intermediate_images[i])
+#     plt.title(intermediate_imagenames[i])
+#     plt.xticks([]),plt.yticks([])
+# plt.show()
 
 
-## Numerics
+#TODO
+# Write the code that will gather 'blob' contours
+# Write code to calculate a bounding box and measure the crystals
+# Enumerate the number of crystals and output some simple statistics (mean size/number of crystals detected etc)
+# Write code which will back-calculate pixel dimensions to actual size measurements.
 
-
-
-
-
-
-
-
-
-
-#
-# # Contour finding
-# cnts = cv2.findContours(eroded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-# cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-#
-# (cnts, _) = contours.sort_contours(cnts)
-#
-#
-# # loop over the contours individually
-# for c in cnts:
-#     # if the contour is not sufficiently large, ignore it
-#     if cv2.contourArea(c) < 10:
-#         continue
-#
-#     # compute the rotated bounding box of the contour
-#     orig = image.copy()
-#     box = cv2.minAreaRect(c)
-#     box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-#     box = np.array(box, dtype="int")
-#
-#     # order the points in the contour such that they appear
-#     # in top-left, top-right, bottom-right, and bottom-left
-#     # order, then draw the outline of the rotated bounding
-#     # box
-#     box = perspective.order_points(box)
-#     cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-#
-#     # loop over the original points and draw them
-#     for (x, y) in box:
-#         cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
-#
-#     # unpack the ordered bounding box, then compute the midpoint
-#     # between the top-left and top-right coordinates, followed by
-#     # the midpoint between bottom-left and bottom-right coordinates
-#     (tl, tr, br, bl) = box
-#     (tltrX, tltrY) = midpoint(tl, tr)
-#     (blbrX, blbrY) = midpoint(bl, br)
-#
-#     # compute the midpoint between the top-left and top-right points,
-#     # followed by the midpoint between the top-righ and bottom-right
-#     (tlblX, tlblY) = midpoint(tl, bl)
-#     (trbrX, trbrY) = midpoint(tr, br)
-#
-#     # draw the midpoints on the image
-#     cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-#     cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-#     cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-#     cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
-#
-#     # draw lines between the midpoints
-#     cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-#              (255, 0, 255), 2)
-#     cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-#              (255, 0, 255), 2)
-#
-#
-#
-#     # compute the Euclidean distance between the midpoints
-#     dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-#     dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))  # compute the size of the object
-#
-#
-# # draw the object sizes on the image
-#     cv2.putText(orig, "{:.1f}in".format(dA),
-#         (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-#         0.65, (255, 255, 255), 2)
-#     cv2.putText(orig, "{:.1f}in".format(dB),
-#         (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-#         0.65, (255, 255, 255), 2)
-#
-# for i in intermediate_images:
-#     cv2.imshow(str(i), i)
-#
-# # show the output image
-# cv2.imshow("Final", orig)
-# cv2.waitKey(0)
+# Eventually refactor the code to be tidier, more function based, faster/more efficient.
